6fa02ecc42a487e4a7cb0d835545abe778b4caf2
hashirkhan6999 <titancaptain51@gmail.com>
2025-03-13 18:20:09 +0500

    Add files via upload

--- project/hashir.js
+++ project/hashir.js
@@ -2,9 +2,4 @@ const signUpButton=document.getElementById('signUpButton');
 const signInButton=document.getElementById('signInButton');
 const signInForm=document.getElementById('signin')
 const signupForm=document.getElementById('signup')
-
-signInButton.addEventListener('click', function(addEventListener)){
-         signInForm.style.display="none";
-         signupForm.style.display="block";
-
-})
+s
